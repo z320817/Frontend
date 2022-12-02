0516d954e8df14ea38c37d0e37988869493dd558
Yosef Shulman <yosef.shulman@protonmail.com>
2022-12-02 21:10:20 +0200

    Finished UI

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ArticleComponent } from '../article/article.component';
 import { PaginatorComponent } from '../paginator/paginator.component';
 import { MaterialModule } from 'src/app/modules/material/material.module';
+import { MainComponent } from '../main/main.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  declarations: [ArticleComponent, PaginatorComponent],
-  imports: [CommonModule, MaterialModule],
+  declarations: [MainComponent, ArticleComponent, PaginatorComponent],
+  imports: [RouterModule, CommonModule, MaterialModule],
   exports: [ArticleComponent, PaginatorComponent],
 })
 export class SharedModule {}
