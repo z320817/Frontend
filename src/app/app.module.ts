23d29e59c32ff8ca29338cf8a17a9bf712e0e100
Yosef Shulman <yosef.shulman@protonmail.com>
2022-12-02 18:18:29 +0200

    Article Component Styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './modules/material/material.module';
-import { MainComponent } from './components/main/main.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchModule } from './modules/search/search.module';
 
 @NgModule({
-  declarations: [AppComponent, MainComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/modules/search/search.module.ts
+++ src/app/modules/search/search.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
 import { MaterialModule } from '../material/material.module';
+import { ArticleComponent } from 'src/app/components/article/article.component';
 
 @NgModule({
-  declarations: [SearchComponent],
+  declarations: [SearchComponent, ArticleComponent],
   imports: [CommonModule, MaterialModule],
 })
 export class SearchModule {}
