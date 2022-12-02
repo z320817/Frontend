4bf98a4cce08f82080af5b1aeef1e6ecbddaa388
Yosef Shulman <yosef.shulman@protonmail.com>
2022-12-02 16:54:08 +0200

    Header added to UI

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, MatToolbarModule],
+  exports: [MatToolbarModule],
 })
-export class MaterialModule { }
+export class MaterialModule {}
