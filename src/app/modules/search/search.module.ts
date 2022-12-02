d066020f649a0ac0399c0b7af4f2bb0970062600
Yosef Shulman <yosef.shulman@protonmail.com>
2022-12-02 18:52:21 +0200

    Initial Paginator Component Style

--- src/app/modules/search/search.module.ts
+++ src/app/modules/search/search.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { SearchComponent } from './search.component';
 import { MaterialModule } from '../material/material.module';
 import { ArticleComponent } from 'src/app/components/article/article.component';
+import { PaginatorComponent } from 'src/app/components/paginator/paginator.component';
 
 @NgModule({
-  declarations: [SearchComponent, ArticleComponent],
+  declarations: [SearchComponent, ArticleComponent, PaginatorComponent],
   imports: [CommonModule, MaterialModule],
 })
 export class SearchModule {}
