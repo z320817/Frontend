a36c89296c78889968b23fe77b51b47d85f2dcf7
Yosef Shulman <yosef.shulman@protonmail.com>
2022-12-03 22:03:20 +0200

    Unit test for app component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,15 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MaterialModule } from './modules/material/material.module';
+import { ArticleModule } from './pages/article/article.module';
+import { BookmarkModule } from './pages/bookmark/bookmark.module';
+import { FetchModule } from './pages/fetch/fetch.module';
+import { SearchModule } from './pages/search/search.module';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MaterialModule,
+        SearchModule,
+        FetchModule,
+        BookmarkModule,
+        ArticleModule,
+        HttpClientModule
       ],
       declarations: [
-        AppComponent
+        AppComponent, HeaderComponent
       ],
     }).compileComponents();
   });
@@ -20,16 +39,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Frontend'`, () => {
+  it(`should have as title 'LandcareResearch Gnews Search'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Frontend');
+    expect(app.title).toEqual('LandcareResearch Gnews Search');
   });
 
-  it('should render title', () => {
+  it('should render News Searcher header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Frontend app is running!');
+    expect(compiled.querySelector('.mat-elevation-z6 span')?.textContent).toContain('News Searcher');
   });
 });
