8539813db4a92e37a896fca2224809b7ba02344e
Yosef Shulman <yosef.shulman@protonmail.com>
2022-12-03 11:45:05 +0200

    Initial Search and Fetch Components Logic

--- src/app/pages/fetch/fetch.component.ts
+++ src/app/pages/fetch/fetch.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { RestApiService } from 'src/app/services/http/rest-api.service';
+import { Article } from 'src/app/shared/interfaces/article';
 
 @Component({
   selector: 'app-fetch',
   templateUrl: './fetch.component.html',
-  styleUrls: ['./fetch.component.scss']
+  styleUrls: ['./fetch.component.scss'],
 })
-export class FetchComponent {
+export class FetchComponent implements OnInit, OnDestroy {
+  public articles: Article[] = [];
+  private subscription: Subscription = new Subscription();
 
+  constructor(public restApiServise: RestApiService) {}
+
+  ngOnInit() {
+    this.fetchArticles();
+  }
+
+  fetchArticles() {
+    this.subscription = this.restApiServise.getArticles().subscribe({
+      next: (response: Article[]) => {
+        this.articles = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { RestApiService } from 'src/app/services/http/rest-api.service';
+import { Article } from 'src/app/shared/interfaces/article';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnDestroy {
+  public articles: Article[] = [];
+  private subscription: Subscription = new Subscription();
 
+  constructor(public restApiServise: RestApiService) {}
+
+  searchArticles(term: string) {
+    this.subscription = this.restApiServise.searchArticles(term).subscribe({
+      next: (response: Article[]) => {
+        this.articles = response;
+      },
+      error: (error) => {
+        console.log(error);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
